89ff5d2465ce3f37ebaf9788220bd5d9d25416eb
Vijay-we <vijayuppala919@gmail.com>
2025-10-26 20:28:05 -0400

    feat(js-basics): added code for variables, arithmetic, and strings practice

---
+++ week-1-js-basics/index.js
@@ -0,0 +1,25 @@
+// Week 1 – Day 1 & 2 (JavaScript Basics)
+
+// Variable declarations and assignments
+let studlyCapVar;
+let properCamelCase;
+let titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Arithmetic practice
+let sum = 10 + 5;
+let difference = 20 - 10;
+let product = 4 * 5;
+let quotient = 10 / 2;
+
+// Increment / Decrement
+sum++;
+product--;
+
+// String practice
+let myName = "Vijay";
+let greeting = "Hello, " + myName + "!";
+console.log(greeting);
